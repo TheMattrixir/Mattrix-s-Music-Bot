9b246bfb32cfd796608ea47b0f8edb280638bec9
TheMattrixir <32433121+TheMattrixir@users.noreply.github.com>
2017-10-01 20:38:06 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.363580088459198464();
 
 client.on('ready', () => {
     console.log('I am ready!');
